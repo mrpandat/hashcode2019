76eed95bf55369ecd35f58d3ebdfc73f0995a62a
Lucas Moisan <lmoisan.ext@wedigital.garden>
2019-02-28 19:25:03 +0100

    output

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from inblc import read_input
 from outblc import write_output
 import sys
 import os
+import pprint
 
 
 def do_something(env):
+    for photo in env['photos']:
+        slide = dict()
+        slide['photos'] = list()
+        slide['photos'].append(photo['id'])
+        env['slides'].append(slide)
+    # pprint.pprint(env)
     return env
 
 
--- outblc.py
+++ outblc.py
@@ -1,4 +1,8 @@
 def write_output(file, env):
     with open(file, 'w') as output_file:
-        line = 'mdr'
-        output_file.write(line)
+        output_file.write(str(len(env['slides'])) + '\n')
+        for slide in env['slides']:
+            for index, photo in enumerate(slide['photos']):
+                slide['photos'][index] = str(photo)
+            line = ' '.join(slide['photos']) + '\n'
+            output_file.write(line)
